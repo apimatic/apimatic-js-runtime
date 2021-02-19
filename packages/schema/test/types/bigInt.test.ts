c8b5cf5f4865212005536fa0405d2d76f92b6ae7
Wajahat Iqbal <wajahat.iqbal@apimatic.io>
2021-02-19 17:35:21 +0500

    Add tests for big int validation

---
+++ packages/schema/test/types/bigInt.test.ts
@@ -0,0 +1,88 @@
+import { bigint, validateAndMap, validateAndUnmap } from '../../src';
+describe('bigint', () => {
+  describe('Mapping', () => {
+    it('should accept bigInt', () => {
+      const input = BigInt(132321);
+      const schema = bigint();
+      const output = validateAndMap(input, schema);
+      expect(output.errors).toBeFalsy();
+      expect((output as any).result).toBe(input);
+    });
+
+    it('should accept numeric bigInt', () => {
+      const input = BigInt('132321');
+      const schema = bigint();
+      const output = validateAndMap(input, schema);
+      expect(output.errors).toBeFalsy();
+      expect((output as any).result).toBe(input);
+    });
+
+    it('should fail on other types', () => {
+      const input = true;
+      const schema = bigint();
+      const output = validateAndMap(input as any, schema);
+      expect((output as any).result).toBeUndefined();
+      expect(output.errors).toHaveLength(1);
+      expect(output.errors).toMatchInlineSnapshot(`
+        Array [
+          Object {
+            "branch": Array [
+              true,
+            ],
+            "message": "Expected value to be of type 'bigint' but found 'boolean'.
+
+        Given value: true
+        Type: 'boolean'
+        Expected type: 'bigint'",
+            "path": Array [],
+            "type": "bigint",
+            "value": true,
+          },
+        ]
+      `);
+    });
+  });
+
+  describe('Unmapping', () => {
+    it('should accept number', () => {
+      const input = BigInt(123123);
+      const schema = bigint();
+      const output = validateAndUnmap(input, schema);
+      expect(output.errors).toBeFalsy();
+      expect((output as any).result).toBe(input);
+    });
+
+    it('should accept numeric string', () => {
+      const input = BigInt('123123');
+      const schema = bigint();
+      const output = validateAndUnmap(input as any, schema);
+      expect(output.errors).toBeFalsy();
+      expect((output as any).result).toBe(input);
+    });
+
+    it('should fail on other types', () => {
+      const input = true;
+      const schema = bigint();
+      const output = validateAndUnmap(input as any, schema);
+      expect((output as any).result).toBeUndefined();
+      expect(output.errors).toHaveLength(1);
+      expect(output.errors).toMatchInlineSnapshot(`
+        Array [
+          Object {
+            "branch": Array [
+              true,
+            ],
+            "message": "Expected value to be of type 'bigint' but found 'boolean'.
+
+        Given value: true
+        Type: 'boolean'
+        Expected type: 'bigint'",
+            "path": Array [],
+            "type": "bigint",
+            "value": true,
+          },
+        ]
+      `);
+    });
+  });
+});
