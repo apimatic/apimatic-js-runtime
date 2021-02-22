b5ba15002d58500831777a220a1cbcaeba4fd4d9
Wajahat Iqbal <wajahat.iqbal@apimatic.io>
2021-02-22 19:43:51 +0500

    Rename and update the tests

--- packages/schema/test/types/bigInt.test.ts
+++ packages/schema/test/types/bigInt.test.ts
@@ -1,7 +1,7 @@
 import { bigint, validateAndMap, validateAndUnmap } from '../../src';
 describe('bigint', () => {
   describe('Mapping', () => {
-    it('should accept bigInt', () => {
+    it('should accept bigint', () => {
       const input = BigInt(9532532599932);
       const schema = bigint();
       const output = validateAndMap(input, schema);
@@ -9,7 +9,7 @@ describe('bigint', () => {
       expect((output as any).result).toBe(input);
     });
 
-    it('should accept negative bigInt', () => {
+    it('should accept negative bigint', () => {
       const input = BigInt(-9532532599932);
       const schema = bigint();
       const output = validateAndMap(input, schema);
@@ -17,20 +17,20 @@ describe('bigint', () => {
       expect((output as any).result).toBe(input);
     });
 
-    it('should accept numeric bigInt', () => {
-      const input = BigInt('9532532599932');
+    it('should accept bigint string', () => {
+      const input = '9532532599932';
       const schema = bigint();
-      const output = validateAndMap(input, schema);
+      const output = validateAndMap(input as any, schema);
       expect(output.errors).toBeFalsy();
-      expect((output as any).result).toBe(input);
+      expect((output as any).result).toBe(BigInt(input));
     });
 
-    it('should accept negative numeric bigInt', () => {
-      const input = BigInt('-9532532599932');
+    it('should accept negative bigint string', () => {
+      const input = '-9532532599932';
       const schema = bigint();
-      const output = validateAndMap(input, schema);
+      const output = validateAndMap(input as any, schema);
       expect(output.errors).toBeFalsy();
-      expect((output as any).result).toBe(input);
+      expect((output as any).result).toBe(BigInt(input));
     });
 
     it('should fail on other types', () => {
@@ -60,7 +60,7 @@ describe('bigint', () => {
   });
 
   describe('Unmapping', () => {
-    it('should accept number', () => {
+    it('should accept bigint', () => {
       const input = BigInt(9532532599932);
       const schema = bigint();
       const output = validateAndUnmap(input, schema);
@@ -68,7 +68,7 @@ describe('bigint', () => {
       expect((output as any).result).toBe(input);
     });
 
-    it('should accept negative number', () => {
+    it('should accept negative bigint', () => {
       const input = BigInt(-9532532599932);
       const schema = bigint();
       const output = validateAndUnmap(input, schema);
@@ -76,20 +76,20 @@ describe('bigint', () => {
       expect((output as any).result).toBe(input);
     });
 
-    it('should accept numeric string', () => {
-      const input = BigInt('9532532599932');
+    it('should accept bigint string', () => {
+      const input = '9532532599932';
       const schema = bigint();
       const output = validateAndUnmap(input as any, schema);
       expect(output.errors).toBeFalsy();
-      expect((output as any).result).toBe(input);
+      expect((output as any).result).toBe(BigInt(input));
     });
 
-    it('should accept negative numeric string', () => {
-      const input = BigInt('-9532532599932');
+    it('should accept negative bigint string', () => {
+      const input = '-9532532599932';
       const schema = bigint();
       const output = validateAndUnmap(input as any, schema);
       expect(output.errors).toBeFalsy();
-      expect((output as any).result).toBe(input);
+      expect((output as any).result).toBe(BigInt(input));
     });
 
     it('should fail on other types', () => {
