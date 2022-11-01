0d8858a6a6a675c67b53bf450adb3fe34c63d2a1
MaryamAdnan3 <maryam.adnan@apimatic.com>
2022-11-01 15:27:46 +0500

    complete branch coverage

--- packages/core/test/fileWrapper.test.ts
+++ packages/core/test/fileWrapper.test.ts
@@ -7,18 +7,32 @@ import {
 import fs from 'fs';
 describe('isFileWrapper', () => {
   it('should verify the instance of FileWrapper', () => {
-    const fileWrapper = new FileWrapper(fs.createReadStream('dummy_file'));
-    expect(isFileWrapper(fileWrapper)).not.toBeFalsy();
+    const fileWrapper = new FileWrapper(
+      fs.createReadStream('test/dummy_file.txt')
+    );
+    expect(isFileWrapper(cloneFileWrapper(fileWrapper))).not.toBeFalsy();
   });
-  it('should verify the instance of FileWrapper after clonning', () => {
+  it('should verify the instance of FileWrapper after clonning with headers', () => {
+    const options: FileWrapperOptions = {
+      contentType: 'optional-content-type',
+      filename: 'dummy_file',
+      headers: { 'test header': 'test value' },
+    };
+    const fileWrapper = new FileWrapper(
+      fs.createReadStream('test/dummy_file.txt'),
+      options
+    );
+    expect(isFileWrapper(cloneFileWrapper(fileWrapper))).not.toBeFalsy();
+  });
+  it('should verify the instance of FileWrapper after clonning without headers', () => {
     const options: FileWrapperOptions = {
       contentType: 'optional-content-type',
       filename: 'dummy_file',
     };
     const fileWrapper = new FileWrapper(
-      fs.createReadStream('dummy_file'),
+      fs.createReadStream('test/dummy_file.txt'),
       options
     );
-    expect(cloneFileWrapper(fileWrapper)).not.toBeFalsy();
+    expect(isFileWrapper(cloneFileWrapper(fileWrapper))).not.toBeFalsy();
   });
 });
