4fd56a3ec8ac150c595dbfadad20a31e394c03cd
Muhammad Haseeb <muhammmadhaseebisb@gmail.com>
2024-04-15 18:04:29 +0500

    added logging config into api logger

--- packages/core/src/logger/apiLogger.ts
+++ packages/core/src/logger/apiLogger.ts
@@ -6,17 +6,20 @@ import {
   Level,
 } from '../coreInterfaces';
 import { CONTENT_LENGTH_HEADER, getHeader } from '../http/httpHeaders';
+import { LoggingConfiguration } from './loggerConfiguration';
 
 export class ApiLogger implements ApiLoggerInterface {
   private readonly _logger: LoggerInterface;
+  private readonly _loggingConfig: LoggingConfiguration;
 
-  constructor(logger: LoggerInterface) {
+  constructor(logger: LoggerInterface, loggingConfig: LoggingConfiguration) {
     this._logger = logger;
+    this._loggingConfig = loggingConfig;
   }
 
   public logRequest(scopeId: string, request: HttpRequest): void {
     this._logger.log(
-      Level.Info,
+      this._loggingConfig.level ?? Level.Info,
       `Request ${scopeId} ${request.method} ${request.url}`,
       { scopeId, method: request.method, url: request.url } // move to one object and
     );
@@ -26,7 +29,7 @@ export class ApiLogger implements ApiLoggerInterface {
     const contentLength = getHeader(response.headers, CONTENT_LENGTH_HEADER);
 
     this._logger.log(
-      Level.Info,
+      this._loggingConfig.level ?? Level.Info,
       `Response ${scopeId} ${response.statusCode} ${contentLength}`,
       {
         scopeId,
@@ -35,4 +38,14 @@ export class ApiLogger implements ApiLoggerInterface {
       }
     );
   }
+}
+
+export function generateGUID(): string {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    /* tslint:disable:no-bitwise */
+    const r = (Math.random() * 16) | 0;
+    /* tslint:disable:no-bitwise */
+    const v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
