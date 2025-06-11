1e77f45fd7e56de85e1d519c679bf775274c911b
mrafnadeem-apimatic <muhammad.nadeem@apimatic.io>
2025-06-11 14:40:15 +0300

    test: numberEnum

---
+++ packages/schema/test/types/numberEnum.test.ts
@@ -0,0 +1,21 @@
+import { numberEnum } from "../../src";
+
+describe('Number Enum', () => {
+  enum SampleNumberEnum {
+    A = 2,
+    B = 4,
+    C = 8,
+    D = 16
+  }
+
+  describe('To JSON Schema', () => {
+    it('should output a valid JSON Schema', () => {
+      const schema = numberEnum(SampleNumberEnum);
+      const jsonSchema = schema.toJSONSchema();
+
+      expect(jsonSchema).toStrictEqual({
+        enum: [2, 4, 8, 16],
+      });
+    });
+  });
+});
