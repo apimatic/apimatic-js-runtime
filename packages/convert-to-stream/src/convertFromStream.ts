7aa7b1d1185d2a985e970fecbb5436415fefecfd
Ayeshas09 <asurryya@gmail.com>
2025-07-10 11:17:11 +0500

    fix(core): remove usage of stream module

---
+++ packages/convert-to-stream/src/convertFromStream.ts
@@ -0,0 +1,16 @@
+export async function convertFromStream(
+  content: string | Blob | NodeJS.ReadableStream
+): Promise<string> {
+  if (typeof content === 'string') {
+    return content;
+  }
+  if (content instanceof Blob) {
+    throw new Error('Type must be ReadableStream');
+  }
+
+  const chunks: Uint8Array[] = [];
+  for await (const chunk of content) {
+    chunks.push(Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk));
+  }
+  return Buffer.concat(chunks).toString();
+}
--- packages/convert-to-stream/src/index.ts
+++ packages/convert-to-stream/src/index.ts
@@ -1,1 +1,2 @@
 export * from './convertToStream';
+export * from './convertFromStream';
