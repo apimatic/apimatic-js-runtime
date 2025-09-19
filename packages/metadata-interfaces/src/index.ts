ba849a11cb55316e860162b17ef5e25d9650f0a2
mehnoorsiddiqui <ayaankhan7532@gmail.com>
2025-09-19 17:02:50 +0500

    refactor: remove .js extensions from imports in index.ts

--- packages/metadata-interfaces/src/index.ts
+++ packages/metadata-interfaces/src/index.ts
@@ -1,3 +1,3 @@
-export * from './endpointMetadataFromId.js';
-export * from './coreClient.js';
+export * from './endpointMetadataFromId';
+export * from './coreClient';
 export { type JSONSchema } from '@apimatic/schema';
