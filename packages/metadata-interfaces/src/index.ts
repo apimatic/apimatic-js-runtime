1ec599c985b308dee0cb3c938a7574e9b6c6faac
mehnoorsiddiqui <ayaankhan7532@gmail.com>
2025-09-22 11:14:59 +0500

    refactor: remove unnecessary file checks from CI workflow and update TypeScript version

--- packages/metadata-interfaces/src/index.ts
+++ packages/metadata-interfaces/src/index.ts
@@ -1,3 +1,7 @@
-export * from './coreClient.js';
-export * from './endpointMetadataFromId.js';
+export type {
+  EndpointMetadataInterface,
+  RequestSchemaInterface,
+  EndpointsObject,
+} from './endpointMetadataFromId.js';
+export type { CoreClient } from './coreClient.js';
 export { type JSONSchema } from '@apimatic/schema';
